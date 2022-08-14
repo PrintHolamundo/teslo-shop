ec7ea2b174f88a9053e02905ca0d6fcf85c2ff65
PrintHolamundo <jdohca@gmail.com>
2022-08-14 09:29:54 -0500

    Create Product DTO

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,38 @@
-export class CreateProductDto {}
+import {
+    IsArray,
+    IsIn,
+    IsInt,
+    IsOptional,
+    IsPositive,
+    IsString,
+    MinLength,
+} from 'class-validator';
+
+export class CreateProductDto {
+    @IsString()
+    @MinLength(1)
+    title: string;
+
+    @IsString()
+    @IsOptional()
+    price?: number;
+
+    @IsString()
+    @IsOptional()
+    description?: string;
+
+    @IsInt()
+    @IsPositive()
+    @IsOptional()
+    slug?: string;
+
+    @IsOptional()
+    stock?: number;
+
+    @IsString({ each: true })
+    @IsArray()
+    sizes: string[];
+
+    @IsIn(['men', 'women', 'kid', 'unisex'])
+    gender: string;
+}
