521d942757f313b4a70512c2d479e45964dd6bf9
PrintHolamundo <jdohca@gmail.com>
2022-08-15 14:07:37 -0500

    ProductImage Entity

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,12 +3,13 @@ import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { Product } from './entities/product.entity';
+import { ProductImage } from './entities/product-image.entity';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
   imports:[
-    TypeOrmModule.forFeature([Product]) 
+    TypeOrmModule.forFeature([Product, ProductImage]) 
   ]
 })
 export class ProductsModule {}
