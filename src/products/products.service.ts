79f2c1f7bf851d71d45572b28528d872db959563
PrintHolamundo <jdohca@gmail.com>
2022-08-16 15:16:31 -0500

    Crear imagenes de producto

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -13,6 +13,7 @@ import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
 import { Product } from './entities/product.entity';
 import { validate as isUUID } from 'uuid';
+import { ProductImage } from './entities/product-image.entity';
 
 @Injectable()
 export class ProductsService {
@@ -21,14 +22,21 @@ export class ProductsService {
   constructor(
     @InjectRepository(Product)
     private readonly productRepository: Repository<Product>,
+
+    @InjectRepository(ProductImage)
+    private readonly productImageRepository: Repository<ProductImage>,
   ) { }
 
   async create(createProductDto: CreateProductDto) {
     try {
-      const product = this.productRepository.create(createProductDto);
+      const {images= [], ...productDetails}= createProductDto;
+      const product = this.productRepository.create({
+        ...productDetails,
+        images: images.map(image => this.productImageRepository.create({url:image}))
+      });
       await this.productRepository.save(product);
 
-      return product;
+      return {...product, images};
     } catch (error) {
       this.handleDBExceptions(error);
     }
@@ -68,6 +76,7 @@ export class ProductsService {
     const product = await this.productRepository.preload({
       id: id,
       ...updateProductDto,
+      images:[]
     });
 
     if (!product)
