2c6849415ab4e973b2eacf2a03b884813c378aa1
PrintHolamundo <jdohca@gmail.com>
2022-08-29 11:45:50 -0500

    Validar archivos

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -3,17 +3,26 @@ import {
   Post,
   UploadedFile,
   UseInterceptors,
+  BadRequestException
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { FilesService } from './files.service';
+import { fileFilter } from './helpers/fileFilter.helper';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post('product')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(FileInterceptor('file' ,{
+    fileFilter: fileFilter
+  }))
   uploadProductImage(@UploadedFile() file: Express.Multer.File) {
-    return file;
+
+    if (!file){
+      throw new BadRequestException('Make sure that the file is an image')
+    }
+
+    return file.originalname;
   }
 }
