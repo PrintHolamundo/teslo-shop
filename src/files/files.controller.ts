053f664f76c5f3238c53a9cbf07a742e896db9ad
PrintHolamundo <jdohca@gmail.com>
2022-09-01 10:21:36 -0500

    Servir archivos de manera controlada

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -3,9 +3,13 @@ import {
   Post,
   UploadedFile,
   UseInterceptors,
-  BadRequestException
+  BadRequestException,
+  Get,
+  Param,
+  Res,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Response } from 'express';
 import { diskStorage } from 'multer';
 import { FilesService } from './files.service';
 import { fileFilter } from './helpers/fileFilter.helper';
@@ -15,21 +19,32 @@ import { fileNamer } from './helpers/fileNamer.helper';
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName')
+    imageName: string,
+  ) {
+    const path = this.filesService.getStaticProductImage(imageName);
+    res.sendFile(path);
+  }
   @Post('product')
-  @UseInterceptors(FileInterceptor('file' ,{
-    fileFilter: fileFilter,
-    // limits: {fieldSize: 1000}
-    storage: diskStorage({
-      destination: './static/products',
-      filename: fileNamer
-    })
-  }))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+      // limits: {fieldSize: 1000}
+      storage: diskStorage({
+        destination: './static/products',
+        filename: fileNamer,
+      }),
+    }),
+  )
   uploadProductImage(@UploadedFile() file: Express.Multer.File) {
-
-    if (!file){
-      throw new BadRequestException('Make sure that the file is an image')
+    if (!file) {
+      throw new BadRequestException('Make sure that the file is an image');
     }
-
-    return file.originalname;
+    // const secureUrl = `${file.filename}`;
+    const secureUrl = `http://localhost:3000/api/files/product/2472d2ee-ca1f-42ee-b6b2-624b169cb256.png`;
+    return secureUrl;
   }
 }
