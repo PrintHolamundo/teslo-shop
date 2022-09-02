d0a4dd920603eaf56fd76a7b62e78da5edc0df78
PrintHolamundo <jdohca@gmail.com>
2022-09-02 10:25:08 -0500

    Entidad de Usuarios

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column({ type: 'text', unique: true })
+  email: string;
+  @Column('text')
+  password: string;
+  @Column('text')
+  fullName: string;
+  @Column({ type: 'bool', unique: true })
+  isActive: boolean;
+  @Column({ type: 'text', array: true, default: ['user'] })
+  roles: string[];
+}
