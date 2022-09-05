3ade4c1d50af61da6ceec1d8154bf3daca7f9f04
PrintHolamundo <jdohca@gmail.com>
2022-09-05 18:04:54 -0500

    Generar un JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,17 +5,20 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { JwtService } from '@nestjs/jwt';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { LoginUserDto } from './dto/login-user.dto';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
+    private readonly jwtService: JwtService,
   ) {}
   async create(createUserDto: CreateUserDto) {
     try {
@@ -25,7 +28,8 @@ export class AuthService {
         password: await bcrypt.hashSync(password, 10),
       });
       await this.userRepository.save(user);
-      return user;
+      delete user.password;
+      return { ...user, token: this.getJwtToken({ email: user.email }) };
     } catch (error) {
       this.handleDBErrors(error);
     }
@@ -39,12 +43,15 @@ export class AuthService {
     });
     if (!user)
       throw new UnauthorizedException('Credentials are not valid (email)');
-    if (bcrypt.compareSync(password, user.password))
+    if (!bcrypt.compareSync(password, user.password))
       throw new UnauthorizedException('Credentials are not valid (password)');
-    return user;
+    return { ...user, token: this.getJwtToken({ email: user.email }) };
     //TODO: retornar el JWT
   }
-
+  private getJwtToken(payload: JwtPayload) {
+    const token = this.jwtService.sign(payload);
+    return token;
+  }
   private handleDBErrors(error: any): never {
     if (error.code === '23505') throw new BadRequestException(error.detail);
     console.log(error);
--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -1,4 +1,10 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity('users')
 export class User {
@@ -14,4 +20,12 @@ export class User {
   isActive: boolean;
   @Column({ type: 'text', array: true, default: ['user'] })
   roles: string[];
+  @BeforeInsert()
+  checkFieldsBeforeInsert() {
+    this.email = this.email.toLowerCase().trim();
+  }
+  @BeforeUpdate()
+  checkFieldsBeforeUpdate() {
+    this.checkFieldsBeforeInsert();
+  }
 }
