b2e4945c95f5a1b313370d05b5565b81e30697be
PrintHolamundo <jdohca@gmail.com>
2022-09-05 12:55:41 -0500

    Modulos Asincronos

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
 import { User } from './entities/user.entity';
+import { AuthController } from './auth.controller';
 
 @Module({
   controllers: [AuthController],
@@ -12,11 +13,23 @@ import { User } from './entities/user.entity';
   imports: [
     TypeOrmModule.forFeature([User]),
     PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: {
+            expiresIn: '2h',
+          },
+        };
+      },
+    }),
     // JwtModule.register({
-    //   secret: process.env.JWT_SECRET,
-    //   signOptions: {
-    //     expiresIn: '2h',
-    //   },
+    // secret: process.env.JWT_SECRET,
+    // signOptions: {
+    //   expiresIn: '2h',
+    // },
     // }),
   ],
   exports: [TypeOrmModule],
