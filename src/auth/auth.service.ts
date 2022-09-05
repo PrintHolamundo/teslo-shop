12dd403c33f35b28528c4a3cdb0df303b98ada33
PrintHolamundo <jdohca@gmail.com>
2022-09-05 09:52:59 -0500

    Encriptar la contrasena

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,6 +5,7 @@ import {
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 
@@ -16,7 +17,11 @@ export class AuthService {
   ) {}
   async create(createUserDto: CreateUserDto) {
     try {
-      const user = this.userRepository.create(createUserDto);
+      const { password, ...userData } = createUserDto;
+      const user = this.userRepository.create({
+        ...userData,
+        password: await bcrypt.hashSync(password, 10),
+      });
       await this.userRepository.save(user);
       return user;
     } catch (error) {
