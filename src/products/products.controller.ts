0b43a29c8a7c73102f765daa69aa334b5e618aae
PrintHolamundo <jdohca@gmail.com>
2022-09-27 11:02:16 -0500

    Auth en otros modulos

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -3,37 +3,42 @@ import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from './../common/dtos/pagination.dto';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ValidRoles } from 'src/auth/interfaces/valid-roles';
 
 @Controller('products')
 export class ProductsController {
-  constructor(private readonly productsService: ProductsService) {}
+  constructor(private readonly productsService: ProductsService) { }
 
   @Post()
+  @Auth(ValidRoles.admin)
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
   @Get()
-  findAll( @Query() paginationDto:PaginationDto ) {
+  findAll(@Query() paginationDto: PaginationDto) {
     // console.log(paginationDto)
-    return this.productsService.findAll( paginationDto );
+    return this.productsService.findAll(paginationDto);
   }
 
   @Get(':term')
-  findOne(@Param( 'term' ) term: string) {
-    return this.productsService.findOnePlain( term );
+  findOne(@Param('term') term: string) {
+    return this.productsService.findOnePlain(term);
   }
 
   @Patch(':id')
+  @Auth(ValidRoles.admin)
   update(
-    @Param('id', ParseUUIDPipe ) id: string, 
+    @Param('id', ParseUUIDPipe) id: string,
     @Body() updateProductDto: UpdateProductDto
   ) {
-    return this.productsService.update( id, updateProductDto );
+    return this.productsService.update(id, updateProductDto);
   }
 
   @Delete(':id')
-  remove(@Param('id', ParseUUIDPipe ) id: string) {
-    return this.productsService.remove( id );
+  @Auth(ValidRoles.admin)
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.productsService.remove(id);
   }
 }
